9f992f9a823c3dc1e92d6592a33802d3f768bd77
Felipe Alzate Gil <felipegil444@gmail.com>
2024-09-05 11:05:52 -0500

    changess

--- src/modules/MainPage/components/NavBar.jsx
+++ src/modules/MainPage/components/NavBar.jsx
@@ -1,17 +1,5 @@
 import logoBlanco from "@/assets/img/logo-blanco.png";
-import { useState, useEffect } from "react";
-
 export default function NavBar() {
-  const [session, setSession] = useState(null); // Initially, session is null
-
-  useEffect(() => {
-    const sessionData = localStorage.getItem("session");
-    if (sessionData) {
-      const session = JSON.parse(sessionData);
-      setSession(session); // Set session directly
-    }
-  }, []);
-
   return (
     <nav className="bg-secondary sticky top-0 w-full z-20 start-0 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-3 py-2">
@@ -27,36 +15,20 @@ export default function NavBar() {
           </span>
         </a>
         <div className="flex gap-1 md:gap-2">
-          {!session ? (
-            <>
-              <a
-                href="/auth/iniciarsesion"
-                type="button"
-                className="text-black bg-white focus:outline-none font-bold rounded-lg text-sm md:text-base px-3 py-2 md:px-4 md:py-2 text-center"
-              >
-                Iniciar sesión
-              </a>
-              <a
-                href="/auth/registrarse"
-                type="button"
-                className="text-black bg-white focus:outline-none font-bold rounded-lg text-sm md:text-base px-3 py-2 md:px-4 md:py-2 text-center"
-              >
-                Registrarse
-              </a>
-            </>
-          ) : session.role === "Coordinador" ? (
-            <a className="text-black bg-white focus:outline-none font-bold rounded-lg text-sm md:text-base px-3 py-2 md:px-4 md:py-2 text-center">
-              Coordinador
-            </a>
-          ) : session.role === "Instructor" ? (
-            <a className="text-black bg-white focus:outline-none font-bold rounded-lg text-sm md:text-base px-3 py-2 md:px-4 md:py-2 text-center">
-              Instructor
-            </a>
-          ) : (
-            <a className="text-black bg-white focus:outline-none font-bold rounded-lg text-sm md:text-base px-3 py-2 md:px-4 md:py-2 text-center">
-              Usuario
-            </a>
-          )}
+          <a
+            href="/auth/iniciarsesion"
+            type="button"
+            className="text-black bg-white focus:outline-none font-bold rounded-lg text-sm md:text-base px-3 py-2 md:px-4 md:py-2 text-center"
+          >
+            Iniciar sesión
+          </a>
+          <a
+            href="/auth/registrarse"
+            type="button"
+            className="text-black bg-white focus:outline-none font-bold rounded-lg text-sm md:text-base px-3 py-2 md:px-4 md:py-2 text-center"
+          >
+            Registrarse
+          </a>
         </div>
       </div>
     </nav>
